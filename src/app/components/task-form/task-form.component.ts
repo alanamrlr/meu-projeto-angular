c56bfb6d4c61e12daea103ca18cbf0efd21b82c8
Alana Rodrigues <alana.menezes.rodrigue@gmail.com>
2025-05-07 16:31:59 +0100

    atualização task-form

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,11 +1,21 @@
+import { TaskService } from './../../services/task.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
-export class TaskFormComponent {
 
+export class TaskFormComponent {
+  newTaskTitle: string ="";
+
+  constructor(private taskService: TaskService){}
+
+  addTask(): void{
+    if(this.newTaskTitle.trim()){
+      this.taskService.addTask(this.newTaskTitle.trim());
+      this.newTaskTitle="";
+    }
+  }
 }
