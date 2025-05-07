b9432b13619afa3ca2ddc5653d3d860133bf5016
Alana Rodrigues <alana.menezes.rodrigue@gmail.com>
2025-05-07 16:43:56 +0100

    tentativa de correção task-form.component.html

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.css'
+  styleUrls: ['./task-form.component.css']
 })
 
 export class TaskFormComponent {
